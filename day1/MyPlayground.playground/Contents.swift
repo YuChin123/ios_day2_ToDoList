7fa7fe838432cf802fc4fee8f6bf705563599fa6
Cyberjaya 4 iTrain <cyberjaya4itrain@Cyberjayas-iMac-6.local>
2017-11-16 15:01:27 +0800

    first commit

---
+++ day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+/*
+var str = "Hello, playground"
+
+print (str)
+
+let myName = "hello"
+let greetings = "good morning" + myName
+
+print(greetings)
+*/
+ 
+ 
+ /*
+let motivated = true
+let motivationMessage = "gambateh "
+let goodMessage :String = "you can do it "
+
+if motivated {
+    print (goodMessage)
+}
+else {
+    print (motivationMessage)
+}
+*/
+
+
+var studentMarks = [9,2,01,50]
+
+studentMarks.count
+
+studentMarks.append(30)
+
+print ("for the third value is \(studentMarks[2])")
+
